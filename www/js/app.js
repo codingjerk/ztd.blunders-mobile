5454987341d465a52be5575569b6fc58b56de8ef
indesege <indesege@gmail.com>
2015-09-12 04:53:49 +0300

    Login page view ready

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,26 @@
-// Ionic Starter App
+var app = angular.module('Ztd.Blunders Mobile', ['ionic']);
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+app.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider.state('login', {
+        url: '/login',
+        templateUrl: 'login.html',
+        controller: 'LoginCtrl'
+    });
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+    $stateProvider.state('training', {
+        url: '/training',
+        templateUrl: 'training.html',
+        controller: 'TrainingCtrl'
+    });
+
+    $urlRouterProvider.otherwise('/login');
+});
+
+app.controller('LoginCtrl', function($scope, $state, $ionicSlideBoxDelegate) {
+    $scope.login = function() {
+        $state.go('training');
+    };
+});
+
+app.controller('TrainingCtrl', function($scope, $state) {
+});
