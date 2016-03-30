424ce09828fee1896b866e7e425c48482ec46368
Evgeny Shtranvasser <jackalsh@gmail.com>
2016-03-30 16:42:32 +0300

    Comment added

--- www/js/buffer.js
+++ www/js/buffer.js
@@ -1,3 +1,9 @@
++/*
++  The idea is, that direct and pack modules provides completely the same
++  functionality from perspective of board module and just can be replaced
++*/
+
+
 var direct = {};
 
 (function(module) {
