87f42b26cd6b61aa06910de35a59780dcbf4c2d7
Evgeny Shtranvasser <jackalsh@gmail.com>
2016-08-19 17:43:03 +0300

    Adding code to install FS adapter for LokiJS during build script

--- build/build.sh
+++ build/build.sh
@@ -9,6 +9,10 @@
 rm -f build/*.apk
 
 cordova build --release android
+# Enabling FS adapter for LokiJS
+# If not installed well, will not show permission for Local storage during installation
+bower install lokijs --save
+cordova plugin add cordova-plugin-file
 
 cp ./platforms/android/build/outputs/apk/android-release-unsigned.apk build/
 
