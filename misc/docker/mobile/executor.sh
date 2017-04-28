e3735adbdfc05afb3694b033ab85def657687dfe
Evgeny Shtranvasser <jackalsh@gmail.com>
2017-04-28 18:18:45 +0300

    Refactoring in docker scripts to build project into conteiner and expose it with nginx

--- misc/docker/mobile/executor.sh
+++ misc/docker/mobile/executor.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-PROJECT_DIR=/root/blunders/ztd.blunders-mobile
+PROJECT_DIR=/home/blunders/ztd.blunders-mobile
 
 MODE=$1
 
 function help {
-  echo 'local - build a project and copies web version to build/'
+  echo 'local - build a project and use nginx to show it'
   echo 'apk   - build an APK and copies it to build/'
   echo 'help  - show this help'
 }
@@ -16,13 +16,12 @@ fi
 
 if [ ${MODE} = "local" ]; then
   cd ${PROJECT_DIR} && \
-  git pull && \
-  ionic setup sass && \
-  rsync -ar --progress --delete ${PROJECT_DIR}/www/ /root/build/
+  git pull
+  exec sleep 100000
+  exec /usr/sbin/nginx -c ${PROJECT_DIR}/misc/docker/mobile/nginx/nginx.conf -g "daemon off;"
 elif [ ${MODE} = "apk" ]; then
   cd ${PROJECT_DIR} && \
   git pull && \
-  ionic setup sass && \
   echo 'Not implemented yet'
 else
   help
