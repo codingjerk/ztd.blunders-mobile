4ba0583de8c206358d2b2bc7a2e090b656645d55
Evgeny Shtranvasser <jackalsh@gmail.com>
2017-05-12 13:51:13 +0300

    Improving Dockerfile

--- misc/docker/mobile/executor.sh
+++ misc/docker/mobile/executor.sh
@@ -14,14 +14,20 @@ if [ -z "${MODE}" ]; then
   help
 fi
 
-if [ ${MODE} = "local" ]; then
-  cd ${PROJECT_DIR} && \
-  git pull
-  echo "Starting local server on port 80"
+cd ${PROJECT_DIR} && \
+   git pull
+
+if [ ${MODE} = "web" ]; then
+  echo "Starting local server on port 8080"
+  echo "Check http://localhost:8080 to access it"
+  rsync -avr --delete ${PROJECT_DIR}/ /tmp/build/
   exec /usr/sbin/nginx -c ${PROJECT_DIR}/misc/docker/mobile/nginx/nginx.conf -g "daemon off;"
+elif [ ${MODE} = "local" ]; then
+  rsync -avr ${PROJECT_DIR}/ /tmp/build/
+  echo "Check build directory to access built project"
+  echo "Do not restart the container as it will forcelly override the content"
+  echo "Commit first !!!"
 elif [ ${MODE} = "apk" ]; then
-  cd ${PROJECT_DIR} && \
-  git pull && \
   echo 'Not implemented yet'
 else
   help
