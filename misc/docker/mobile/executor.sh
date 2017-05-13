0fe54c2e6cad9fac4299a130bc4588b2fb93a1af
Evgeny Shtranvasser <jackalsh@gmail.com>
2017-05-14 01:31:09 +0300

    Finishing APK deploy script

--- misc/docker/mobile/executor.sh
+++ misc/docker/mobile/executor.sh
@@ -3,12 +3,33 @@
 PROJECT_DIR=/home/blunders/ztd.blunders-mobile
 
 MODE=$1
+PASSPHRASE=$2
 
 function help {
+  echo 'web   - access on port 8080 by web server from within a container' 
   echo 'local - build a project and use nginx to show it'
   echo 'apk   - build an APK and copies it to build/'
   echo 'help  - show this help'
 }
+
+for i in "$@"
+do
+case $i in
+    -m=*|--mode=*)
+      export MODE="${i#*=}"
+      shift # past argument=value
+      ;;
+    -p=*|--passphrase=*)
+      export PASSPHRASE="${i#*=}"
+      shift # past argument=value
+      ;;
+    *)
+      echo "Unknown option met: ${i}"
+      help
+      exit 1
+      ;;
+esac
+done
 
 if [ -z "${MODE}" ]; then
   help
@@ -28,7 +49,19 @@ elif [ ${MODE} = "local" ]; then
   echo "Do not restart the container as it will forcelly override the content"
   echo "Commit first !!!"
 elif [ ${MODE} = "apk" ]; then
-  echo 'Not implemented yet'
+  if [ -z "${PASSPHRASE}" ]; then
+    echo "Passphrase is missed"
+    exit 1
+  fi
+  KEY=/run/secrets/free.keystore
+  ZIPALIGN=$(find /opt/AndroidSDK/ -name zipalign)
+
+  cordova build --release android && \
+  cp ./platforms/android/build/outputs/apk/android-release-unsigned.apk ./ && \
+  echo "${PASSPHRASE}" | jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ${KEY} ./android-release-unsigned.apk alias_name && \
+  ${ZIPALIGN} -v 4 ./android-release-unsigned.apk ./blunders-signed.apk && \
+  rsync -av ./blunders-signed.apk /tmp/build/
+  
 else
   help
 fi
